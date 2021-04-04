d5b6b0e9dbe55c0be6c04d0e1459c741ca5173e4
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-04 16:19:13 -0500

    Setup basic structure and connected Mongoose to MongoDB

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("Connection open")
+    })
+    .catch(err => {
+        console.log(`Oh no, error: ${err}`)
+    })
