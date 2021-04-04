74cf3a558680bd6ceba9052b82d359736bff28dc
Jon Smith <aardvarkjon94@yahoo.com>
2021-04-04 17:05:37 -0500

    Created first Schema; Saved movie in node shell

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
     .catch(err => {
         console.log(`Oh no, error: ${err}`)
     })
+
+
+//Representational of the type of data I want to send 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+//Mongoose will create a collection by default. Add in the desired model name and the schema
+const Movie = mongoose.model('Movie', movieSchema);
+
+const avengers = new Movie({ title: 'The Avengers', year: 2012, score: 9.7, rating: 'PG-13' });
